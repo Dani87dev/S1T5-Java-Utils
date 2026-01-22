12e536baabe3aa9a6d198f0b13acc41fd8cd0c41
Daniel Vilà <danivila.dev@gmail.com>
2026-01-22 12:28:09 +0100

    feat: refactor task 4 level 1 with the same structure of tasks 1, 2 and 3

--- src/main/java/n1exercise4/Exercise4.java
+++ src/main/java/n1exercise4/Exercise4.java
@@ -1,32 +1,60 @@
 package n1exercise4;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Date;
 
 public class Exercise4 {
 
     public static void main(String[] args) {
-        validateArguments(args);
 
-        String filePath = args[0];
-        File file = new File(filePath);
+        String directoryPath = "docs" + File.separator + "example_directory";
+        String txtFileDirectoryPath ="docs" + File.separator + "example_directory" + File.separator + "a_document.txt";
 
-        validateFile(file);
+        File folder = new File(directoryPath);
+        File textDocument = new File(txtFileDirectoryPath);
 
-        readTXT(file);
+        validateDirectory(folder);
+
+        try (FileWriter writer = new FileWriter("directory_Tree.txt")) {
+            listDirectoryWritter(folder, writer);
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing file");
+        }
+
+        readTXT(textDocument);
+
     }
 
-    private static void validateArguments(String[] args) {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("a TXT file path as a parameter isn't found");
+    private static void validateDirectory(File folder) {
+        if (!folder.exists() || !folder.isDirectory()) {
+            throw new IllegalArgumentException("Isn't a valid directory");
         }
     }
 
-    private static void validateFile(File file) {
-        if (!file.exists() || !file.isFile()) {
-            throw new IllegalArgumentException("Is not a valid file");
+    private static File[] getDirectoryFiles(File folder) {
+        File[] list = folder.listFiles();
+        if (list == null) {
+            throw new IllegalStateException("Cannot read directory contents");
+        }
+        Arrays.sort(list);
+        return list;
+    }
+
+    private static void printDirectory(File file, FileWriter writer) throws IOException {
+        String typeFile = file.isDirectory() ? "(D)" : "(F)";
+        String lastModified = new Date(file.lastModified()).toString();
+        writer.write(typeFile + " " + file.getName() + " - " + lastModified + "\n");
+    }
+
+
+    private static void listDirectoryWritter(File folder, FileWriter writer) throws IOException {
+        for (File file : getDirectoryFiles(folder)) {
+            printDirectory(file, writer);
+            if (file.isDirectory()) {
+                writer.write("\n");
+                listDirectoryWritter(file, writer);
+            }
         }
     }
 
@@ -40,4 +68,6 @@ public class Exercise4 {
             throw new RuntimeException("Error reading file");
         }
     }
+
+
 }
